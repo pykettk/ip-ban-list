48fe230b6f692a4d4ef049b2548ac732b11c4a21
Kiel Pykett <pykettk@gmail.com>
2020-07-16 11:36:09 +0100

    Update registration.php

--- registration.php
+++ registration.php
@@ -3,6 +3,6 @@ use Magento\Framework\Component\ComponentRegistrar;
 
 ComponentRegistrar::register(
     ComponentRegistrar::MODULE,
-    'Element119_ModuleName',
+    'Element119_IpBlacklist',
     __DIR__
 );
