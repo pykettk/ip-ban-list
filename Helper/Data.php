19c3f30da85dd4e1af3df34bcaece8c3481a34ad
Kiel Pykett <pykettk@gmail.com>
2020-07-16 12:21:45 +0100

    Add Common Helper Functions
    + Function to check whether the module is enabled
    + Function to retrieve the customer's IP address

--- Helper/Data.php
+++ Helper/Data.php
@@ -3,16 +3,43 @@ namespace Element119\IpBanList\Helper;
 
 use Magento\Framework\App\Helper\AbstractHelper;
 use Magento\Framework\App\Helper\Context;
+use Magento\Framework\HTTP\PhpEnvironment\RemoteAddress;
 
 class Data extends AbstractHelper
 {
+    const MODULE_ENABLED_CONFIG = 'ip_ban_list/general/enable';
+
+    /** @var RemoteAddress */
+    protected $remoteAddress;
+
     /**
      * Data constructor.
      *
      * @param Context $context
+     * @param RemoteAddress $remoteAddress
      */
-    public function __construct(Context $context)
-    {
+    public function __construct(
+        Context $context,
+        RemoteAddress $remoteAddress
+    ) {
         parent::__construct($context);
+
+        $this->remoteAddress = $remoteAddress;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isModuleEnabled()
+    {
+        return $this->scopeConfig->isSetFlag(self::MODULE_ENABLED_CONFIG);
+    }
+
+    /**
+     * @return bool|string
+     */
+    public function getCustomerIp()
+    {
+        return ($this->isModuleEnabled()) ? $this->remoteAddress->getRemoteAddress() : false;
     }
 }
